695727c21bcfd268f5145019567cb2e98a80be20
Anil <web.dev@bluelakes-technology.com>
2022-11-30 16:29:51 +0530

    design changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,6 +8,34 @@ import { CommonModule } from '@angular/common';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
 
+export class HeaderComponent {
+  menuItems:any=[];
+  constructor() {
+    this.menuItems = [
+      {
+          name: 'Build A Dream',
+          url: '/build-a-dream'
+      },
+      
+      {
+          name: 'Inspire',
+          url: '/resources'
+      },
+      {
+          name: 'Community',
+          url: '/my-account'
+      },
+      {
+        name: 'My Dreams',
+        url: '/dreams'
+    },
+      {
+          name: 'Login',
+          url: '/layout'
+      }
+  ];
+  }
+  
+  
 }
