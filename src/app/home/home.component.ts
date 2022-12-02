2b21ecc6b8aa35a805083b3167a577ec248c9adc
Anil <web.dev@bluelakes-technology.com>
2022-12-02 14:14:16 +0530

    Home process mortagage section design

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatExpansionModule } from '@angular/material/expansion';
 declare var $: any;
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatExpansionModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 
 export class HomeComponent {
- 
+  panelOpenState = false;
 }
